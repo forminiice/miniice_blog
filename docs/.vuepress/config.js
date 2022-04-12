121e7f070ff8a27473ca9c73665f969d92c9b88b
ice <miniice@iice.fun>
2022-04-12 21:36:25 +0800

    blog初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 站点配置
+  lang: 'zh-CN',
+  title: 'ice',
+  description: '小冰啦小冰',
+
+  // 主题和它的配置
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+}
