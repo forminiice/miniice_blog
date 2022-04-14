71ffd08b7eaeaaa6f9286e335c239407868f8237
ice <miniice@iice.fun>
2022-04-14 22:53:02 +0800

    修改跳转

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <h1>去找优总啦。。。</h1>
     <Content />
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    window.location.href="http://124.221.56.58/"
+    window.history.replaceState()
+  }
+}
+</script>
+
+<style>
+</style>
