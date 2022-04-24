e39086db8bfa410040da095c39be99d4e1013f32
ice <miniice@iice.fun>
2022-04-24 23:49:39 +0800

    删除友链配置

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,11 +1,3 @@
-const links = require('./links') // 引入友链配置
-
 module.exports = {
-  logo: '/img/logo/ico.png',
-  navbar: [
-    {
-      text: '友链',
-      link: '/links',
-    },
-  ],
+  logo: '/img/logo/ico.png'
 }
